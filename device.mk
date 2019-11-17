3a9759d3ba53758dbd91617517468a354ce34f19
Davide Garberi <dade.garberi@gmail.com>
2019-11-17 17:50:05 -0500

    ginkgo: Update thermal HAL to v2.0 mock implementation
    Change-Id: I6696db69b3aa34012ebff41d1304bffab7f5b00c

--- device.mk
+++ device.mk
@@ -455,8 +455,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
+    android.hardware.thermal@2.0-service.mock \
     thermal.trinket
 
 # Touchscreen
