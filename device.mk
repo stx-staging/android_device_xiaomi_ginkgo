da4b7b179d8aef03509ce9129be0a5feeb8d662c
Call190Now <call190now@outlook.com>
2020-05-04 01:52:13 -0300

    ginkgo: remove a little more lineage stuff

--- device.mk
+++ device.mk
@@ -26,11 +26,7 @@ PRODUCT_BOARD_PLATFORM := msmnile
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
-
 PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
