ba344ce8e8ffd0b3372b415e381c4d3d9e2548c9
Call190Now <call190now@outlook.com>
2020-05-05 04:04:22 -0300

    ginkgo: enable namespaces
    turns out anxcam were affecting other devices, so we'll enable namespaces

--- device.mk
+++ device.mk
@@ -477,6 +477,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    device/xiaomi/ginkgo
+
 # WiFi Display
 PRODUCT_PACKAGES += \
     libdisplayconfig.vendor \
