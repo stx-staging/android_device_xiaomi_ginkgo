trinket-devs/device_xiaomi_ginkgo

ginkgo: Add DT2W support for PA

device.mk
@@ -21,7 +21,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-pa
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal

stx-staging/android_device_xiaomi_ginkgo

device.mk
@@ -25,7 +25,8 @@ PRODUCT_BOARD_PLATFORM := msmnile
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-statix
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 